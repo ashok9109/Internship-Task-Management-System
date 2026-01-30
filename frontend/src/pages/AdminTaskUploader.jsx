25b5df9ecf21035994cddd0888dd99b247087926
ashok9109 <aaaaashokyadav123@gmail.com>
2026-01-30 21:24:17 +0530

    feat: update styling for AdminTaskUploader component and improve button accessibility

--- frontend/src/pages/AdminTaskUploader.jsx
+++ frontend/src/pages/AdminTaskUploader.jsx
@@ -165,7 +165,7 @@ const AdminTaskUploader = () => {
                             </textarea>
                         </div>
                         <button disabled={taskDetailsLoading}
-                            className='w-full bg-sky-500 rounded py-2 text-black' type='submit' >
+                            className='w-full bg-sky-500 rounded py-2 text-white' type='submit' >
                             {taskDetailsLoading ? "....Creating Task Just Wait" : "Create Task"}
                         </button>
                     </form>
@@ -173,33 +173,27 @@ const AdminTaskUploader = () => {
             </section>
 
             {/* page2 code */}
-            <section className='flex flex-col space-y-3' >
+            <section className='flex flex-col space-y-3 text-white' >
 
                 <div className='h-full w-full flex flex-col md:flex-row gap-20 p-4' >
 
                     {/* Left div */}
                     <div className='w-full max-w-5xl flex flex-col items-center justify-center bg-slate-900/50 shadow shadow-lg shadow-sky-500 border-2 border-sky-500 rounded-lg space-y-6 p-3' >
-                        <h1 className='text-white text-sm font-bold font1' >1. 📊 Upload Tasks via Excel</h1>
-                        <p className='text-sm text-slate-400 font5' >Upload multiple internship tasks at once using an <span className='text-sky-500 text-sm font-bold ' >Excel File.</span></p>
-                        <p className='text-sm text-slate-400 font5' >Ensure the Excel file follows the predefined format to <span className='text-sky-500 text-sm font-bold ' >Avoid errors.</span></p>
-
-                        <ul className='list-disc list-inside text-white text-sm' >
-                            <li className='text-sm' >Allowed Format <b>xlsx</b>, <b>.csv</b></li>
-                            <li>Make sure each task as unique number <b><span className='text-sky-500' >Task Number</span></b></li>
-                        </ul>
+                        <h1 className='font1' >1. 📊 Upload Tasks via Excel</h1>
+                        <p>Upload multiple internship tasks at once using an <span className='text-sky-500' >Excel File.</span></p>
 
                         {/* This is the form for uploading Excel and csv */}
                         <form onSubmit={uploadImageHandler} className='w-full space-y-2 flex flex-col space-y-10' >
 
                             {/* Excel file uploading input */}
-                            <label className=' text-sm text-sky-500' >Upload the excel</label>
-                            <label className='w-full border-2 flex itmes-center justify-center border-sky-500 border-dashed rounded-sm p-6 hover:scale-[0.9] transition' >
+                            <label>Upload the excel</label>
+                            <label className='w-full border-2 flex itmes-center justify-center border-sky-500 border-dashed rounded-sm p-6' >
                                 <h1 className='text-sm text-slate-500' >Drag and Drop</h1>
                                 <input onChange={(e) => setImageUploadFile(e.target.files[0] || null)} className='hidden' accept='.xlsx,.xlx,.csv' type="file" />
                             </label>
 
                             <button disabled={uploadImageLoading}
-                                type='submit' className='bg-sky-500 rounded py-2 shadow-sm shadow-sky-500' >
+                                type='submit' className='bg-sky-500 rounded py-2' >
                                 {uploadImageLoading ? "....Uploading Image just Wait" : "Upload Image"}
                             </button>
                         </form>
@@ -207,31 +201,26 @@ const AdminTaskUploader = () => {
 
                     {/* Right div */}
                     <div className='w-full max-w-5xl flex flex-col items-center justify-center bg-slate-900/50 shadow shadow-lg shadow-sky-500 border-2 border-sky-500 rounded-lg space-y-7 p-3' >
-                        <h1 className='text-white text-sm font-bold font1' >2. Add the task code for interns</h1>
-                        <p className='text-sm text-slate-400 font5 ' >This task code is help the interns to complete the task and add internship task with the <span className='text-sky-500 text-sm font-bold ' >Task Number</span></p>
-
-                        <ul className='list-disc list-inside text-white text-sm' >
-                            <li className='text-sm' >Allowed Format <b>code</b>, <b>text</b></li>
-                            <li>Make sure each task as unique number <b><span className='text-sky-500' >Task Number</span></b></li>
-                        </ul>
+                        <h1>2. Add the task code for interns</h1>
+                        <p>This task code is help the interns to complete the task and add internship task with the <span className='text-sky-500 text-sm font-bold ' >Task Number</span></p>
 
                         {/* this is the form for uploading */}
                         <form onSubmit={taskCodeHandler} className='w-full space-y-2 flex flex-col ' >
 
                             {/* upload task number */}
                             <label className='text-sm text-sky-500' > Add Task Number</label>
-                            <label className='w-full border-2 flex itmes-center justify-center border-sky-500 border-dashed rounded-sm p-1 hover:scale-[0.9] transition' >
+                            <label className='w-full border-2 flex itmes-center justify-center border-sky-500 border-dashed rounded-sm p-1' >
                                 <input onChange={(e) => setCodeTaskNumber(e.target.value)} value={codeTaskNumber} className='w-full p-2 outline-0 text-white' type="text" placeholder='Task-01' />
                             </label>
 
                             {/* uplaod image input field */}
-                            <label className=' text-sm text-sky-500' > Add the task code </label>
-                            <label className='w-full border-2 flex itmes-center justify-center border-sky-500 border-dashed rounded-sm hover:scale-[0.9] transition' >
+                            <label className='text-sm text-sky-500' > Add the task code </label>
+                            <label className='w-full border-2 flex itmes-center justify-center border-sky-500 border-dashed rounded-sm ' >
                                 <input onChange={(e) => setTaskCode(e.target.value)} value={taskCode} className='h-full w-full text-white p-6 outline-0' placeholder='Add Task Code' type="text" />
                             </label>
 
                             <button disabled={taskCodeLoading}
-                                type='submit' className='bg-sky-500 rounded py-2 shadow-sm shadow-sky-500' >
+                                type='submit' className='bg-sky-500 rounded py-2' >
                                 {taskCodeLoading ? "Upoading code just wait" : "Upload Code"}
                             </button>
                         </form>
